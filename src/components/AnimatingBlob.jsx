536ad41ddddc55e9780111b0cb8c5027893d1a4c
Fahad Nisar <fnisar142@gmail.com>
2023-06-09 05:50:01 +0500

    final

--- src/components/AnimatingBlob.jsx
+++ src/components/AnimatingBlob.jsx
@@ -1,5 +1,3 @@
-import Gif_Plasma from "../assets/col/Gif_Plasma.gif";
-
 export default function AnimatingBlob() {
   return (
     <div className="w-[500px] h-full overflow-hidden sm:block hidden -ml-48">
